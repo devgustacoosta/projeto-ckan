b62f8f2af7adc23ee7ce260194bd82221549070f
gustavo_lima <glcosta@almavivasolutions.com.br>
2025-06-13 13:42:15 -0300

    Ultima alterações

--- ckanext/almavivasolutions/controllers.py
+++ ckanext/almavivasolutions/controllers.py
@@ -3,22 +3,27 @@ from flask import Blueprint, render_template, g
 
 bp = Blueprint('almavivasolutions', __name__, url_prefix='/')
 
-@bp.route('/about-us')
-def sobre():
-    return render_template('about-us.html')
+@bp.route('/organization/data-gov-eua')
+def dataGovEUA():
+    return render_template('organization/data-eua.html', page={})
 
-@bp.route('/open-government')
-def openGovernment():
-    return render_template('open-government.html')
+@bp.route('/organization/dados-abertos-goias')
+def dadosGO():
+    return render_template('organization/data-go.html', page={})
 
-@bp.route('/contact')
-def contact():
-    return render_template('contact.html')
 
-@bp.route('/user-guide')
-def userGuide():
-    return render_template('user-guide.html')
+# @bp.route('/open-government')
+# def openGovernment():
+#     return render_template('open-government.html')
 
-@bp.route('/metrics')
-def metrics():
-    return render_template('metrics.html')
+# @bp.route('/contact')
+# def contact():
+#     return render_template('contact.html')
+
+# @bp.route('/user-guide')
+# def userGuide():
+#     return render_template('user-guide.html')
+
+# @bp.route('/metrics')
+# def metrics():
+#     return render_template('metrics.html')
